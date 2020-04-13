b97a194ae88d05eef688e4f3297677ca770fb4d5
julia <olkhovyk_ak16@nuwm.edu.ua>
2020-04-13 16:38:33 +0300

    app

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config():
     FLASK_APP=os.environ.get('FLASK_APP')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
