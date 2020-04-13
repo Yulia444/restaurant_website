3db6e75b04047cd9191122c45a9d0596cfdf3cc3
julia <olkhovyk_ak16@nuwm.edu.ua>
2020-04-13 15:26:30 +0300

    itit app

---
+++ app/config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config():
+    FLASK_APP=os.environ.get('FLASK_APP')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+
+class DevConfig(Config):
+    DEBUG = True
+
+class ProdConfig(Config):
+    DEBUG = False
